4bd7249e3643f6dba81033cc4674451eb915ca94
Marin <marin.sersic@gmail.com>
2017-05-09 23:00:11 +0200

    9.5.2017.

---
+++ XamarinApp/XamarinApp/Resources/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinApp.Resources
+{
+    class Functions
+    {
+        public TimeSpan CalculateTravelTime(DateTime start, DateTime end)
+        {
+            return end.Subtract(start);
+        }
+        public List<Line> ReturnLinesThroughStation(String station)
+        {
+            List<Line> lines = new List<Line>();
+            //...
+            return lines;
+        }
+    }
+}
---
+++ XamarinApp/XamarinApp/Resources/Line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinApp.Resources
+{
+    class Line
+    {
+        String lineName;
+        List<DateTime> startTimes;
+        List<DateTime> travelTimes;
+        List<String> stations;
+    }
+}
